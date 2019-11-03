f145729ce2df9aafa0f5f91db9b2d0c8fb19797d
kamilest <stankeviciute.kamile@gmail.com>
2019-11-03 10:48:35 +0000

    [Rosalind] BA7F backtrack attempt.

--- BA7F/ba7f.py
+++ BA7F/ba7f.py
@@ -7,6 +7,8 @@ class ParsimonyNode:
     self.tag = tag
     self.characters = characters
 
+    self.min_score = 0
+
     # Stores scores for all the characters rather than running the 
     # algorithm for each position in the string
     self.scores = []
@@ -23,29 +25,60 @@ class ParsimonyNode:
       return True
     return self.left.tag == 1 and self.right.tag == 1
 
+def small_parsimony(node):
+  small_parsimony_bottom_up(node)
+  small_parsimony_top_down(node)
 
-
-def small_parsimony(root):
+def small_parsimony_bottom_up(node):
   def delta(i, k):
     return 0 if i == k else 1
   
-  if root is None or root.left is None or root.right is None:
+  if node is None or node.left is None or node.right is None:
     return
 
-  small_parsimony(root.left)
-  small_parsimony(root.right)
-  root.tag = 1
+  # Postorder traversal
+  small_parsimony_bottom_up(node.left)
+  small_parsimony_bottom_up(node.right)
+  node.tag = 1
 
-  for i in range(len(root.left.scores)):
-    root.scores.append({})
+  for i in range(len(node.left.scores)):
+    node.scores.append({})
 
-  for ix, s in enumerate(root.scores):
+  for ix, s in enumerate(node.scores):
     for k in 'ACTG':
-      si = [root.left.scores[ix][i] + delta(i, k) for i in 'ACTG']
-      sj = [root.right.scores[ix][j] + delta(j, k) for j in 'ACTG']
-      print(si, sj)
-
+      si = [node.left.scores[ix][i] + delta(i, k) for i in 'ACTG']
+      sj = [node.right.scores[ix][j] + delta(j, k) for j in 'ACTG']
+      
       s[k] = min(si) + min(sj)
+
+def small_parsimony_top_down(node):
+  if node is None or node.left is None or node.right is None:
+    return
+  
+  min_scores = []
+  for s in node.scores:
+    sc = [s[k] for k in 'ACTG']
+    sc_ix = np.argmin(sc)
+    min_scores.append(sc[sc_ix])
+    node.characters += 'ACTG'[sc_ix]
+  
+  node.min_score = np.sum(min_scores)
+
+  small_parsimony_top_down(node.left)
+  small_parsimony_top_down(node.right)
+
+def print_small_parsimony(T):
+  def label_delta(characters_i, characters_j):
+    sum = 0
+    for i, j in zip(characters_i, characters_j):
+      sum += 0 if i == j else 1
+    return sum 
+
+  print(T[max(T)].min_score)
+  for k in reversed(sorted(T.keys())):
+    if T[k].left is not None and T[k].right is not None:
+      print("{}->{}:{}".format(T[k].left.characters, T[k].characters, label_delta(T[k].left.characters, T[k].characters)))
+      print("{}->{}:{}".format(T[k].right.characters, T[k].characters, label_delta(T[k].right.characters, T[k].characters)))      
 
 T = {}
 label = 0
@@ -76,9 +109,6 @@ for line in f:
 f.close()
 
 # Assuming maximum key is the root
-small_parsimony(T[max(T)])
-scores = []
-for s in T[max(T)].scores:
-  scores.append(min([s[k] for k in 'ACTG']))
-
-print(np.sum(scores))
+root = T[max(T)]
+small_parsimony(root)
+print_small_parsimony(T)
